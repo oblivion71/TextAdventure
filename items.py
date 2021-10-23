3aa822359781b393c7072ab9b4a3484545c39ed3
oblivion71 <56285046+oblivion71@users.noreply.github.com>
2021-10-23 18:38:20 +1100

    Update items.py
    desc + opray weapon

--- items.py
+++ items.py
@@ -1,17 +1,27 @@
 stick = {
     "name": "Stick",
+    "desc": "The remains of an old tree",
     "atk": 3,
     "equipable": True
 }
 
 wooden_sword = {
     "name": "Wooden_sword",
+    "desc": "a versatile, close contact practice weapon",
     "atk": 5,
     "equipable": True
 }
 
 bow = {
     "name": "Bow",
+    "desc": "An ancient Bow, does it still hold its power?",
     "atk": 7,
+    "equipable": True
+}
+
+debroglie_wavelength = {
+    "name": "debroglie_wavelength",
+    "desc": "The Debroglie Wavelength",
+    "atk": 10,
     "equipable": True
 }
